5134eebfb5547d7dbf72c083d8e3dd1a394a969e
Mruzik1 <40868829+Mruzik1@users.noreply.github.com>
2018-10-26 13:47:29 +0300

    Add files via upload

--- Buble.js
+++ Buble.js
@@ -1,9 +1,10 @@
 var array = [12, 53, -23, 3.1415, -0.5, 354];
 var arrCount = array.length - 1;
+var reverse;
 for (var i = 0; i < arrCount; i++) {
     for (var j = 0; j < arrCount - i; j++) {
         if (array[j] > array[j+1]) {
-            var reverse = array[j];
+            reverse = array[j];
             array[j] = array[j+1];
             array[j+1] = reverse;
         }
